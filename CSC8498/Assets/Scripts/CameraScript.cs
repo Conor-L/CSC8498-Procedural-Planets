d4b2ea854d81ce51aee9e366fe7a7102c7bbfd80
Conor Lambert <lambert.conor123@gmail.com>
2021-05-25 15:15:57 +0100

    Normalized Shader and Triplanar scale

--- CSC8498/Assets/Scripts/CameraScript.cs
+++ CSC8498/Assets/Scripts/CameraScript.cs
@@ -8,10 +8,12 @@ public class CameraScript : MonoBehaviour
     public Camera mainCamera;
     public GameObject planet;
 
+    public PlanetSettings pSettings;
+
     void Update()
     {
         mainCamera.transform.LookAt(planet.transform);
+        mainCamera.transform.position = new Vector3(0, 0, -pSettings.planetRadius * 3);
     }
-
     
 }
